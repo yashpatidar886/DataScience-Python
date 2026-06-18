f1f8e359e6071004b23c9641931ac16a780a287a
yashpatidar886 <patidary276@gmail.com>
2026-06-18 18:43:22 +0530

    Update pratice.py

--- pratice.py
+++ pratice.py
@@ -220,231 +220,231 @@ print("+" + 10 * "-" + "+")
 print(("|" + " " * 10 + "|\n") * 5 , end="")
 print("+" + 10 * "-" + "+")
 
-# city = 'bhopal'
-# print(city[0])
-# print(city[2])
-# print(city[-1])
-# print(city[-5])
+city = 'bhopal'
+print(city[0])
+print(city[2])
+print(city[-1])
+print(city[-5])
 
-# print(2==2)
-# print(2==2.2)
-# var = 0
-# print(var==0)
-# var == 1
-# print(var == 0)
+print(2==2)
+print(2==2.2)
+var = 0
+print(var==0)
+var == 1
+print(var == 0)
 
-# var = 10
-# if var ==11:
-#    print("Var is 11")
-# print("HEllo")   
+var = 10
+if var ==11:
+   print("Var is 11")
+print("HEllo")   
 
-# yash  = 11 
-# if yash==11:
-#     print("yash is 11")
+yash  = 11 
+if yash==11:
+    print("yash is 11")
 
-# num1 = int(input("Enter a First number "))
-# num2 = int(input("Enter a Second  number "))
+num1 = int(input("Enter a First number "))
+num2 = int(input("Enter a Second  number "))
 
-# if num1 > num2 :
-#    larger_num = num1
-# else :
-#    larger_num = num2 
+if num1 > num2 :
+   larger_num = num1
+else :
+   larger_num = num2 
    
-# print("Larger number is : ", larger_num)   
-   
-   
-
-# num1 = int(input("Enter a First number "))
-# num2 = int(input("Enter a Second  number "))
-# num3 = int(input("Enter a Third  number "))
-# if num1 > num2 and  num1 >num3 :larger_num = num1
-# elif   num2 >num3 and num2 >num1: larger_num = num2
-# else :  larger_num = num3
+print("Larger number is : ", larger_num)   
    
    
-# print("Larger number is : ", larger_num)   
+
+num1 = int(input("Enter a First number "))
+num2 = int(input("Enter a Second  number "))
+num3 = int(input("Enter a Third  number "))
+if num1 > num2 and  num1 >num3 :larger_num = num1
+elif   num2 >num3 and num2 >num1: larger_num = num2
+else :  larger_num = num3
    
    
-# num1 = int(input("Enter a First number "))
-# num2 = int(input("Enter a Second  number "))
-# num3 = int(input("Enter a Third  number "))
+print("Larger number is : ", larger_num)   
+   
+   
+num1 = int(input("Enter a First number "))
+num2 = int(input("Enter a Second  number "))
+num3 = int(input("Enter a Third  number "))
 
-# largest_num = max(num1,num2,num3)
-# minmum = min(num1,num2,num3)
-# print("Largest  number is : ", largest_num)  
-# print("Minimum number is  " ,minmum)
+largest_num = max(num1,num2,num3)
+minmum = min(num1,num2,num3)
+print("Largest  number is : ", largest_num)  
+print("Minimum number is  " ,minmum)
 
-# while True:
-#    print("I an Stack in a loop")
+while True:
+   print("I an Stack in a loop")
 
-# largest_number = -99999
-# number = int(input("Enter a number or type -1 to stop"))
+largest_number = -99999
+number = int(input("Enter a number or type -1 to stop"))
 
-# while number !=-1:
-#    if number>largest_number:
-#      largest_number=number
-#      number = int(input("Enter a number or type -1 to stop"))
+while number !=-1:
+   if number>largest_number:
+     largest_number=number
+     number = int(input("Enter a number or type -1 to stop"))
      
-# print("largest number is :",largest_number)     
+print("largest number is :",largest_number)     
 
 
-# user = str(input("Enter a sentence : "))
+user = str(input("Enter a sentence : "))
 
-# if user == "Spathiphyllum":
-#   print("Yes - Spathiphyllum is the best plant ever!")
-# elif user=="spathiphyllum":
-#   print("No, I want a big Spathiphyllum!")
-# else:
-#   print("Spathiphyllum! Not", user + "!")
+if user == "Spathiphyllum":
+  print("Yes - Spathiphyllum is the best plant ever!")
+elif user=="spathiphyllum":
+  print("No, I want a big Spathiphyllum!")
+else:
+  print("Spathiphyllum! Not", user + "!")
 
-# income = float(input("Enter income :"))
+income = float(input("Enter income :"))
 
-# if income<=85528:
-#     tax = income*0.18-556.2
-#     if tax <0:
-#         tax = 0
+if income<=85528:
+    tax = income*0.18-556.2
+    if tax <0:
+        tax = 0
         
-# else:
-#     tax=14839.02+(income-85528)*0.32
-# print("the tax is " , round(tax) , "thalers")     
+else:
+    tax=14839.02+(income-85528)*0.32
+print("the tax is " , round(tax) , "thalers")     
 
-# while number !=-1:
-#    if number>largest_number:
-#      largest_number=number
-#      number = int(input("Enter a number or type -1 to stop"))
+while number !=-1:
+   if number>largest_number:
+     largest_number=number
+     number = int(input("Enter a number or type -1 to stop"))
      
-# print("largest number is :",largest_number)     
+print("largest number is :",largest_number)     
        
 
 
-# number =  int(input("Enter a number : "))
-# even_count  =0
-# odd_count =0
-# while number !=0:
-#     if number % 2==0:
-#         even_count +=1
-#     else:
-#         odd_count +=1  
-#     number =  int(input("Enter a number : "))     
-# print("Even :" , even_count)
-# print("odd :" , odd_count)        
+number =  int(input("Enter a number : "))
+even_count  =0
+odd_count =0
+while number !=0:
+    if number % 2==0:
+        even_count +=1
+    else:
+        odd_count +=1  
+    number =  int(input("Enter a number : "))     
+print("Even :" , even_count)
+print("odd :" , odd_count)        
     
-# num = [1,2,3,4]
-# print(num[3])
-# print(bool(4))
-# print(bool(6))
-# print(bool(-1))
-# print(bool(0))            
+num = [1,2,3,4]
+print(num[3])
+print(bool(4))
+print(bool(6))
+print(bool(-1))
+print(bool(0))            
         
-# print(bool("a"))
-# print(bool("b"))
-# print(bool(" "))
-# print(bool(""))    
+print(bool("a"))
+print(bool("b"))
+print(bool(" "))
+print(bool(""))    
 
-# print(bool())
-# print(bool(None))
-# print(bool(null))    
+print(bool())
+print(bool(None))
+print(bool(null))    
             
-# counter =5
-# while counter!=0:
-#     print("Inside the loop " , counter)
-#     counter -=1
-# print("Outside the loop") 
+counter =5
+while counter!=0:
+    print("Inside the loop " , counter)
+    counter -=1
+print("Outside the loop") 
 
 
-# counter =5 
-# while counter :
-#     print("Inside the loop " , counter)
-#     counter -=1
-# print("Outside the loop")                    
+counter =5 
+while counter :
+    print("Inside the loop " , counter)
+    counter -=1
+print("Outside the loop")                    
 
-# for counter in range(10):
-#     print("counter :" , counter)
+for counter in range(10):
+    print("counter :" , counter)
 
-# for counter in range(2,8):
-#     print("Counter :" , counter)     
+for counter in range(2,8):
+    print("Counter :" , counter)     
 
-# print("The break instructions : ")
-# for counter in range (1,6):
-#     if counter == 3:
-#       break
-#     print("Inside the loop",counter)
-# print("Outside the loop")     
+print("The break instructions : ")
+for counter in range (1,6):
+    if counter == 3:
+      break
+    print("Inside the loop",counter)
+print("Outside the loop")     
     
     
-# print("The break instructions : ")
-# for counter in range (1,6):
-#     if counter == 3:
-#      continue
-#     print("Inside the loop",counter)
-# print("Outside the loop")       
+print("The break instructions : ")
+for counter in range (1,6):
+    if counter == 3:
+     continue
+    print("Inside the loop",counter)
+print("Outside the loop")       
 
-# largest_number = -99999
-# counter =0
+largest_number = -99999
+counter =0
 
-# while True:
-#     number = int(input("Enter a number or type -1 to end the program "))
-#     if number == -1:
-#         break
-#     counter += 1
-#     if number > largest_number:
-#         largest_number = number
-# if counter !=0:
-#     print("The largest number is " , largest_number)
-# else:
-#     print("You haven't enter a number")            
+while True:
+    number = int(input("Enter a number or type -1 to end the program "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+if counter !=0:
+    print("The largest number is " , largest_number)
+else:
+    print("You haven't enter a number")            
         
         
 largest_number =-9999999
 counter = 0
 
-# while number != -1:
-#     if number ==-1:
-#         continue
-#     counter +=1
+while number != -1:
+    if number ==-1:
+        continue
+    counter +=1
     
-#     if number > largest_number:
-#          largest_number = number
-#     number = int(input("Enter a number or type -1 to end the program "))
+    if number > largest_number:
+         largest_number = number
+    number = int(input("Enter a number or type -1 to end the program "))
     
-# if counter:
-#     print("The largest number is " , largest_number)
-# else:
-#     print("You haven't enter a number")                  
+if counter:
+    print("The largest number is " , largest_number)
+else:
+    print("You haven't enter a number")                  
 
-# counter =1 
-# while counter <5:
-#     print(counter)
-#     counter +=1
-# else:
-#     print("ELse" , counter)    
+counter =1 
+while counter <5:
+    print(counter)
+    counter +=1
+else:
+    print("ELse" , counter)    
     
-# counter =5
-# while counter <5:
-#     print(counter)
-#     counter +=1
-# else:
-#     print("ELse" , counter)    
+counter =5
+while counter <5:
+    print(counter)
+    counter +=1
+else:
+    print("ELse" , counter)    
 
 
-# for counter in range (5):
-#     print(counter)
-# else:
-#     print("else " , counter)     
+for counter in range (5):
+    print(counter)
+else:
+    print("else " , counter)     
      
-# counter = 111
-# for counter in range (2,1):
-#    print(counter )
-# else:
-#     print("else", counter )        
+counter = 111
+for counter in range (2,1):
+   print(counter )
+else:
+    print("else", counter )        
 
 
      
-# blocks = int(input("Enter numbber of blocks : "))
-# counter = 0
-# while(blocks - counter > 0):
-#     counter +=1
-#     blocks = blocks - counter 
+blocks = int(input("Enter numbber of blocks : "))
+counter = 0
+while(blocks - counter > 0):
+    counter +=1
+    blocks = blocks - counter 
     
 # print("height of paryamid : " , counter) 
 
