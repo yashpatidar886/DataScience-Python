f5f4ad2bc1c61511d1517979e4b0a7fb964affb5
Yash <patidary276@gamil.com>
2026-06-30 19:49:07 +0530

    Pratice Ques

--- pratice.py
+++ pratice.py
@@ -788,7 +788,13 @@ yash patidar'''
 # print(l)
 
 # l = [i for i in range(1,31) if i%2==0]
-# print(l)
+#print(l)
 
-    
+ 
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for i in range(len(my_list)):
+#  if my_list[i]==5:
+#         print(i) 
+        
 
+        
